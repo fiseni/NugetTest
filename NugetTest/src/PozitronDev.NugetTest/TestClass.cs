f9a863451ec5903b78d1e5645e66a8da4d886a69
Fati Iseni <fati.iseni@pozitrongroup.com>
2020-10-24 15:26:43 +0200

    Initial commit. Added initial solution structure and initial projects.

---
+++ NugetTest/src/PozitronDev.NugetTest/TestClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PozitronDev.NugetTest
+{
+    public class TestClass
+    {
+        public int TestProperty { get; set; }
+    }
+}
---
+++ NugetTestPlugin/src/PozitronDev.NugetTest.Plugin/PluginTestClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PozitronDev.NugetTest.Plugin
+{
+    public class PluginTestClass
+    {
+    }
+}
