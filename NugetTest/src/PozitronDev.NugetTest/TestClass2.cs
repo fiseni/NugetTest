aec285d886062ecd8cb9e7b06684efc4fab814e5
Fati Iseni <fati.iseni@pozitrongroup.com>
2020-10-24 16:33:12 +0200

    Test triggering build for base package only.

---
+++ NugetTest/src/PozitronDev.NugetTest/TestClass2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PozitronDev.NugetTest
+{
+    public class TestClass2
+    {
+        public int TestProperty2 { get; set; }
+    }
+}
